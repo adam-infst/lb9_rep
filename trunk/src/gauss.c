c8a398473ffb8f25e13b24986166fa01db9bebcc
adam-infst <01197874@pw.edu.pl>
2024-12-17 01:40:20 +0100

    Poprawka do funkcji wykonującej eliminację Gaussa (plik się kompilował ale miał kilka błędów)

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -7,31 +7,32 @@
  */
 int eliminate(Matrix *mat, Matrix *b) {
 	int i, j, k;
-	int diagonal_length = mat->r > mat->c ? mat->r : mat->c;
+	int diagonal_length = mat->r < mat->c ? mat->r : mat->c;
 
 	for (i = 0; i < diagonal_length; i++) {
 		double pivot = mat->data[i][i];
 
 		if (pivot == 0) return 1;
 
-		for (j = i+1; j < mat->r; j++) /* zeruj po kolei elementy ponizej diagonalnej z tej kolumny */
+		for (j = i+1; j < mat->r; j++) /* zeruje po kolei elementy ponizej diagonalnej z tej kolumny */
 		{
 			double el_value = mat->data[j][i]; /* element ktory chcemy wyzerowac */
 			if (el_value == 0) continue;
 
-			for (k = 0; k < mat->r; k++)
+			for (k = 0; k < mat->c; k++)
 			{
 				/* za pomoca petli mnozymy caly wiersz pzez pivot */
 				mat->data[j][k] *= pivot;
-				b->data[j][k] *= pivot;
-				/* teraz nowa wartosc elementu jest rowna el_value * pivot, wiec zeby go wyzerowac 
+				/* nowa wartosc elementu bedzie teraz rowna el_value * pivot, wiec zeby go wyzerowac 
 				wystarczy el_value razy odjac wiersz z pivotem */
 				mat->data[j][k] -= mat->data[i][k] * el_value;
-				b->data[j][k] -= b->data[i][k] * el_value;
 			}
+			/* trzeba wykonac ta sama operacje na macierzy zmiennych */
+			b->data[j][0] *= pivot;
+			b->data[j][0] -= b->data[i][0] * el_value;
 		}
 	}
 
 	return 0;
-	/* notatki: macierz 3x3 zaczyna sie mat->dane[0][0] i konczy mat->dane[2][2] */
+	/* wazne: macierz 3x3 zaczyna sie mat->dane[0][0] i konczy mat->dane[2][2] */
 }
