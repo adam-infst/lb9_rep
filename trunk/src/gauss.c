256471a36b6f6609f46a0e06a44580ef7b9a3092
pjarzabs <01181664@pw.edu.pl>
2024-12-19 19:02:51 +0100

    Metoda eliminacji Gaussa, rozszerzona o wybor elementu glownego. Student A, diag_elem1.

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,5 +1,6 @@
 #include "gauss.h"
 #include <stdio.h>
+#include <math.h> //potrzebna do fabs
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -9,7 +10,43 @@ int eliminate(Matrix *mat, Matrix *b) {
 	int i, j, k;
 	int diagonal_length = mat->r < mat->c ? mat->r : mat->c;
 
+
 	for (i = 0; i < diagonal_length; i++) {
+
+        /// ROZSZERZENIE O WYBÓR ELEMENTU GŁÓWNEGO - student A - branch diag_elem1
+
+        // znajduje wiersz z największą wartością (bezwzględną - fabs) elementu w danej kolumnie
+
+        int max_row = i;
+        double max_wart = fabs(mat->data[i][i]);
+
+        for (j = i + 1; j < mat->r; j++) {
+            if (fabs(mat->data[j][i]) > max_wart){
+                max_row = j;
+                max_wart = fabs(mat->data[j][i]);
+            }
+        }
+
+     // jak max_wart == 0, macierz jest osobliwa (nie dalo by sie podzielic przez zero)
+        if (max_wart == 0)
+            return 1;
+
+        // zamiana wierszy, jeśli wiersz z max wartością, nie jest bieżącym wierszem
+        if (max_row != i) {
+            for (k =0; k < mat->c; k++){
+
+                double temp = mat->data[i][k];  //zamiana
+                mat->data[i][k] = mat->data[max_row][k];
+                mat->data[max_row][k] = temp;
+            }
+            // zamiana wierszy, tyle że teraz dla macierzy b (macierz zmiennych)
+            double temp_b = b->data[i][0];
+            b->data[i][0] = b->data[max_row][0];
+            b->data[max_row][0] = temp_b;
+        }
+
+        /// ROZSZERZENIE O WYBÓR ELEMENTU GŁÓWNEGO - koniec zmian.
+
 		double pivot = mat->data[i][i];
 
 		if (pivot == 0) return 1;
@@ -23,7 +60,7 @@ int eliminate(Matrix *mat, Matrix *b) {
 			{
 				/* za pomoca petli mnozymy caly wiersz pzez pivot */
 				mat->data[j][k] *= pivot;
-				/* nowa wartosc elementu bedzie teraz rowna el_value * pivot, wiec zeby go wyzerowac 
+				/* nowa wartosc elementu bedzie teraz rowna el_value * pivot, wiec zeby go wyzerowac
 				wystarczy el_value razy odjac wiersz z pivotem */
 				mat->data[j][k] -= mat->data[i][k] * el_value;
 			}
